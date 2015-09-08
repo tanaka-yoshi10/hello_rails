207f4f168d208ecc738c1467ad8b1fd68a966e34
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-09 01:01:59 +0900

    add relations

--- Gemfile
+++ Gemfile
@@ -43,3 +43,7 @@ group :development, :test do
   gem 'spring'
 end
 
+group :development do
+  gem 'hirb'
+  gem 'hirb-unicode'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903141328) do
+ActiveRecord::Schema.define(version: 20150908134543) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body",       limit: 65535
+    t.string   "status",     limit: 255
+    t.integer  "entry_id",   limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "title",      limit: 255
