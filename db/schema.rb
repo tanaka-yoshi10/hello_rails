30b109702daaac4cb358001a8925b0627b25464c
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-09 07:33:38 +0900

    add blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908134543) do
+ActiveRecord::Schema.define(version: 20150908215200) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       limit: 65535
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150908134543) do
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "blog_id",    limit: 4
   end
 
 end
