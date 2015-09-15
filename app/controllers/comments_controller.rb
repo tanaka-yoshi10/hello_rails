40d88059fc6bfbd182db31991c4a347e3ba77846
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-16 07:28:29 +0900

    親子関係を無視してEntryやCommentを表示、登録できないようにする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:destroy, :approve]
 
   def create
     @entry = Entry.find(params[:entry_id])
@@ -15,13 +15,10 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment.destroy
-    @entry = Entry.find(params[:entry_id])
     redirect_to [@entry.blog, @entry], notice: 'Comment was successfully destroyed.'
   end
 
   def approve
-    @entry = Entry.find(params[:entry_id])
-    @comment = Comment.find(params[:id])
     @comment.status = "approved"
 
     if @comment.save
@@ -34,7 +31,8 @@ class CommentsController < ApplicationController
 
   private
   def set_comment
-    @comment = Comment.find(params[:id])
+    @entry = Entry.find(params[:entry_id])
+    @comment = @entry.comments.find(params[:id])
   end
 
   def comment_params
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -60,8 +60,8 @@ class EntriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
-      @entry = Entry.find(params[:id])
       @blog = Blog.find(params[:blog_id])
+      @entry = @blog.entries.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
