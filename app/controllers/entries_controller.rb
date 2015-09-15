1caf2f272ee9b260763be7fb68ac6289293d726c
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-16 07:04:23 +0900

    enable to update entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -38,7 +38,7 @@ class EntriesController < ApplicationController
   def update
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
+        format.html { redirect_to [@entry.blog, @entry], notice: 'Entry was successfully updated.' }
         format.json { render :show, status: :ok, location: @entry }
       else
         format.html { render :edit }
