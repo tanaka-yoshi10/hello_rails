5fd7807c240d8ea953f5b4201d45173a60290d7c
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-16 07:06:29 +0900

    enable to destory entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -52,7 +52,7 @@ class EntriesController < ApplicationController
   def destroy
     @entry.destroy
     respond_to do |format|
-      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
+      format.html { redirect_to @blog, notice: 'Entry was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
