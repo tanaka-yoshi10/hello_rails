6d85c5f71341194f5cd58232aadc6d29893f99e1
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-16 06:52:13 +0900

    create and destory blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,8 +14,27 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog, notice: 'Blog was successfully created.'
+    else
+      redirect_to render :new
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: 'Blog was successfully destroyed.'
+  end
+
   private
     def set_blog
       @blog = Blog.find(params[:id])
+    end
+
+    def blog_params
+      params.require(:blog).permit(:title)
     end
 end
