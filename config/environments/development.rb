876871c4805527ccdc989a0d49ce9c353974c1dd
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-09-17 07:23:16 +0900

    link url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: "localhost", port: "3000" }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
